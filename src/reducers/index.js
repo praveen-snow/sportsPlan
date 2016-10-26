d087f6a82efb00f6eaee01b8206366698ac0c63e
praveen-snow <praveenkumar.snow@gmail.com>
2016-10-26 17:31:22 -0500

    adding git ignore and few changes in reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,14 +2,10 @@ import { combineReducers } from 'redux';
 //
 
 import app                 from './app';
-import currentProduct      from './currentProduct';
-import header              from './header';
 import navigator           from './navigator';
 
 
 export default combineReducers({
     app,
-    currentProduct,
-    header,
     navigator
 });
