a98bcf557228ce04a03055de0380b91c2017e049
praveen-snow <praveenkumar.snow@gmail.com>
2017-04-15 10:47:33 -0500

    infinte scroll

--- src/components/Backdrop/index.js
+++ src/components/Backdrop/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { bindListener } from 'utils';
 import ss from './styles.scss';
 import PureRenderMixin  from 'react-addons-pure-render-mixin';
-
+import InfiniteScroll from 'react-infinite-scroller';
 export default React.createClass({
     mixins: [PureRenderMixin],
 
@@ -21,10 +21,43 @@ export default React.createClass({
   	componentDidMount() {
 
     },
+    items(){
+
+    },
+    loadFunc(){
+
+    },
     render() {
+      var items = [];
+      let tracks = [
+      {
+          permalink_url:"www.google.com",
+          artwork_url:"./assets/Cristiano Ronaldo.jpg",
+          title:"hello"
+      }
+      ];
+       tracks.map((track, i) => {
+           items.push(
+               <div className="track" key={i}>
+                   <a href={track.permalink_url} target="_blank">
+                       <img src={track.artwork_url} width="150" height="150" />
+                       <p className="title">{track.title}</p>
+                   </a>
+               </div>
+           );
+       });
         return (
             <div className="appBackground">
-                <h1>Welcome to Scholl Book Now </h1>
+                <h1>Welcome to Scholl Book Now</h1>
+                <InfiniteScroll
+                    pageStart={0}
+                    loadMore={this.loadFunc}
+                    hasMore={true || false}
+                    loader={<div className="loader">Loading ...</div>}
+                    useWindow={false}
+                >
+                    {items}
+                </InfiniteScroll>
             </div>
         );
     },
---
+++ src/components/PlayersInfo/TestCom.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ss from './styles.scss';
+export default React.createClass({
+render(){
+  return(
+    <div>{this.props.data}</div>
+  );
+}
+});
