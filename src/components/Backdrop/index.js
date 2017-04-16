20d3c52ee91f6637d669cd0873ca9885591b83e5
praveen-snow <praveenkumar.snow@gmail.com>
2017-04-16 15:12:14 -0500

    infinite scroll implementation

--- src/components/Backdrop/index.js
+++ src/components/Backdrop/index.js
@@ -5,61 +5,98 @@ import { bindListener } from 'utils';
 import ss from './styles.scss';
 import PureRenderMixin  from 'react-addons-pure-render-mixin';
 import InfiniteScroll from 'react-infinite-scroller';
+import qwest from 'qwest';
+
+const imageList = [];
+const api = {
+    baseUrl: 'https://api.soundcloud.com',
+    client_id: 'caf73ef1e709f839664ab82bef40fa96'
+};
+
 export default React.createClass({
-    mixins: [PureRenderMixin],
+  mixins: [PureRenderMixin],
 
 	getInitialState() {
 		return {
+      tracks: [],
+      hasMoreItems: true,
+      nextHref: null
 		};
 	},
-    componentWillMount() {
-        ss.use();
-    },
-    componentWillUnmount() {
-        ss.unuse();
-    },
-  	componentDidMount() {
+  componentWillMount() {
+      ss.use();
+  },
+  componentWillUnmount() {
+      ss.unuse();
+  },
+	componentDidMount() {
 
-    },
-    items(){
+  },
+  loadItems(page) {
+    var self = this;
+    var url = api.baseUrl + '/users/8665091/favorites';
+    if(this.state.nextHref) {
+        url = this.state.nextHref;
+    }
+    qwest.get(url, {
+            client_id: api.client_id,
+            linked_partitioning: 1,
+            page_size: 10
+        }, {
+            cache: true
+        })
+        .then(function(xhr, resp) {
+            if(resp) {
+                var tracks = self.state.tracks;
+                resp.collection.map((track) => {
+                    if(track.artwork_url == null) {
+                        track.artwork_url = track.user.avatar_url;
+                    }
 
-    },
-    loadFunc(){
+                    tracks.push(track);
+                });
 
-    },
-    render() {
-      var items = [];
-      let tracks = [
-      {
-          permalink_url:"www.google.com",
-          artwork_url:"./assets/Cristiano Ronaldo.jpg",
-          title:"hello"
-      }
-      ];
-       tracks.map((track, i) => {
-           items.push(
-               <div className="track" key={i}>
-                   <a href={track.permalink_url} target="_blank">
-                       <img src={track.artwork_url} width="150" height="150" />
-                       <p className="title">{track.title}</p>
-                   </a>
-               </div>
-           );
-       });
-        return (
-            <div className="appBackground">
-                <h1>Welcome to Scholl Book Now</h1>
-                <InfiniteScroll
-                    pageStart={0}
-                    loadMore={this.loadFunc}
-                    hasMore={true || false}
-                    loader={<div className="loader">Loading ...</div>}
-                    useWindow={false}
-                >
-                    {items}
-                </InfiniteScroll>
-            </div>
-        );
-    },
-
+                if(resp.next_href) {
+                    self.setState({
+                        tracks: tracks,
+                        nextHref: resp.next_href
+                    });
+                } else {
+                    self.setState({
+                        hasMoreItems: false
+                    });
+                }
+            }
+        });
+  },
+  render() {
+    var items = [];
+     this.state.tracks.map((track, i) => {
+      items.push(
+        <div className="track" key={i}>
+          <a href={track.permalink_url} target="_blank">
+            <img src={track.artwork_url} width="150" height="150" />
+            <p className="title">{track.title}</p>
+          </a>
+        </div>
+      );
+     });
+     let loader = (<div className="loader">Loading ...</div>);
+    return (
+      <div className="appBackground">
+        <h1>Welcome to Scholl Book Now</h1>
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={this.loadItems}
+          hasMore={this.state.hasMoreItems}
+          loader={loader}
+          useWindow={true}
+        >
+        <div className="tracks">
+          {items}
+        </div>
+        </InfiniteScroll>
+      </div>
+    );
+  },
 });
